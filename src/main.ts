e7d517e233b19fe6f54d81dc1a1a9f7a53697c82
Artsiom Martynau <xzfantom@gmail.com>
2023-07-29 10:33:24 +0200

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Library API')
+    .setDescription('A Library API with CRUD functionality')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
